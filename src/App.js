502e072e84de6f122917457ccccfd3c620fb22f9
Alexander Matulionis <awmatulionis@Alexanders-MacBook-Pro.local>
2019-05-31 06:53:44 -0700

    Solid progress.

--- src/App.js
+++ src/App.js
@@ -6,35 +6,54 @@ import Smile, { setDoc } from './Smile'
 
 function App() {
   const user = useUser()
-  const [ offer, setOffer ] = useState()
+  const [ local, setLocal ] = useState()
+  const [ remote, setRemote ] = useState()
+  const [ ice, setIce ] = useState()
 
   useEffect(() => {
     if (!user) return
 
     return firebase.firestore().collection('webrtc').onSnapshot(snapshot => 
       snapshot.docChanges().forEach(async ({ doc, type }) => {
-        console.log(`handleDoc!`)
-
-        if (doc.id === user.uid) return console.log(`handleDoc: ignoring incoming add; it's ours.`)
+        if (doc.id === user.uid)   return console.log(`handleDoc: ignoring incoming add; it's ours.`)
         if (type !== 'added') return console.log(`handleDoc: ${type} is not a valid type.`)
 
         const msg = doc.data()
         console.log(`handleDoc: ${JSON.stringify(msg, null, 2)}`)
 
-        await doc.ref.delete()
+        // if      (msg.ice)                   await handleIce(pc, msg.ice)
+        // else if (msg.type === 'offer')  await handleOffer(user.uid, pc, msg.sdp, facingMode)
+        // else if (msg.type === 'answer') await handleAnswer(pc, msg.sdp)
+        // else                                return alert(`handleDoc: unknown data.`)
+
+        // return doc.ref.delete()
       })
     )
   }, [user])
 
+  if (!user) return null
+
   return (
     <div>
       <Smile
-        offer={offer}
-        onOffer={offer => {
-          console.log(`Smile onOffer! ${JSON.stringify(offer)}`)
-          setDoc(user.uid, offer)
-          setOffer(offer)
+        onLocalDesc={local => {
+          console.log(`Smile onLocalDesc!`)
+          setDoc(user.uid, local)
+          setLocal(local)
         }}
+        onRemoteDesc={remote => {
+          console.log(`Smile onRemoteDesc!`)
+          setDoc(user.uid, remote)
+          setRemote(remote)
+        }}
+        onIceCand={ice => {
+          console.log(`Smile onIceCand!`)
+          setDoc(user.uid, ice)
+          setIce(ice)
+        }}
+        localDesc={local}
+        remoteDesc={remote}
+        iceCand={ice}
       />
     </div>
   )
--- src/Smile.js
+++ src/Smile.js
@@ -1,24 +1,24 @@
-import React, { createRef, useRef, useState, useEffect } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'webrtc-adapter'
 import styles from './Smile.module.scss'
 
-// handleDoc processes a request from firestore.
-async function handleDoc(uid, doc, type, pc, facingMode) {
-  if (doc.id === uid)   return console.log(`handleDoc: ignoring incoming add; it's ours.`)
-  if (type !== 'added') return console.log(`handleDoc: ${type} is not a valid type.`)
+// // handleDoc processes a request from firestore.
+// async function handleDoc(uid, doc, type, pc, facingMode) {
+//   if (doc.id === uid)   return console.log(`handleDoc: ignoring incoming add; it's ours.`)
+//   if (type !== 'added') return console.log(`handleDoc: ${type} is not a valid type.`)
 
-  const msg = doc.data()
-  console.log(`handleDoc: ${JSON.stringify(msg, null, 2)}`)
+//   const msg = doc.data()
+//   console.log(`handleDoc: ${JSON.stringify(msg, null, 2)}`)
 
-  if      (msg.ice)                   await handleIce(pc, msg.ice)
-  else if (msg.sdp.type === 'offer')  await handleOffer(uid, pc, msg.sdp, facingMode)
-  else if (msg.sdp.type === 'answer') await handleAnswer(pc, msg.sdp)
-  else                                return alert(`handleDoc: unknown data.`)
+//   if      (msg.ice)                   await handleIce(pc, msg.ice)
+//   else if (msg.sdp.type === 'offer')  await handleOffer(uid, pc, msg.sdp, facingMode)
+//   else if (msg.sdp.type === 'answer') await handleAnswer(pc, msg.sdp)
+//   else                                return alert(`handleDoc: unknown data.`)
 
-  return doc.ref.delete()
-}
+//   return doc.ref.delete()
+// }
 
 // setDoc updates firestore with either an offer, answer, or ice request.
 export async function setDoc(uid, msg) {
@@ -38,7 +38,7 @@ async function handleIce(pc, ice) {
 }
 
 // handleOffer processes a WebRTC offer.
-async function handleOffer(uid, pc, sdp, facingMode) {
+async function handleOffer(pc, sdp, facingMode, onLocalDesc) {
   console.warn('handleOffer!')
 
   await pc.current.setRemoteDescription(new RTCSessionDescription(sdp))
@@ -52,7 +52,7 @@ async function handleOffer(uid, pc, sdp, facingMode) {
   await pc.current.setLocalDescription(answer)
     .catch(err => alert(`handleOffer::setLocalDescription: ${err}`))
 
-  return setDoc(uid, { sdp: pc.current.localDescription })
+  onLocalDesc(pc.current.localDescription)
 }
 
 // handleAnswer processes a WebRTC answer.
@@ -68,8 +68,10 @@ async function openCamera(pc, facingMode, cam) {
   if (!navigator.mediaDevices) return alert(`This browser does not support WebRTC. Try using the original browser for this device.`)
   if (!pc.current)             return alert(`RTCPeerConnection lost.`)
 
+  facingMode = facingMode || 'user'
+  
   const stream = await navigator.mediaDevices.getUserMedia({
-    video: { facingMode },
+    video: {facingMode},
     audio: true,
   })
 
@@ -91,24 +93,24 @@ async function openCamera(pc, facingMode, cam) {
 }
 
 // createOffer signals to another user the offer to communicate.
-async function createOffer(pc, offer, onOffer) {
-  if (offer) return
+async function createOffer(pc, localDesc, onLocalDesc) {
+  if (localDesc) return
   
-  const o = await pc.current.createOffer()
+  const offer = await pc.current.createOffer()
     .catch(err => alert(`createOffer: ${err}`))
 
-  await pc.current.setLocalDescription(o)
+  await pc.current.setLocalDescription(offer)
     .catch(err => alert(`setLocalDescription: ${err}`))
 
-  onOffer(pc.current.localDescription)
+  onLocalDesc(pc.current.localDescription)
 }
 
 // handleIceCandidate responds to an incoming ICE candidate.
-async function handleIceCandidate(e, uid) {
+async function handleIceCandidate(e, onIceCand) {
   console.log('handleIceCandidate!')
 
   return e.candidate
-    ? setDoc(uid, { ice: e.candidate })
+    ? onIceCand(e.candidate)
     : console.log(`handleIceCandidate: ice broadcast arrived.`)
 }
 
@@ -118,12 +120,21 @@ function handleTrack(e, cam) {
 }
 
 // Smile is the core component.
-function Smile({ offer, onOffer }) {
+function Smile({
+  onLocalDesc,
+  onRemoteDesc,
+  onIceCand,
+  localDesc,
+  remoteDesc,
+  iceCand,
+}) {
   console.warn(`Smile render!`)
 
   const [facingMode, setFacingMode] = useState('user')
   const pc = useRef()
+  const ourCam = useRef()
   const theirCam = useRef()
+
   useEffect(() => {
     pc.current = new RTCPeerConnection({
       iceServers: [
@@ -131,31 +142,41 @@ function Smile({ offer, onOffer }) {
         { urls: 'stun:stun.l.google.com:19302' },
       ],
     })
-    pc.current.onicecandidate = e => handleIceCandidate(e)
+    pc.current.onicecandidate = e => handleIceCandidate(e, onIceCand)
     pc.current.ontrack        = e => handleTrack(e, theirCam)
 
     // const dc = pc.current.createDataChannel('data')
     // dc.onopen = _ => {
     //   alert('dc.onopen!')
     // }
-
-    // return firebase.firestore().collection('webrtc').onSnapshot(snapshot => 
-    //   snapshot.docChanges().forEach(async ({ doc, type }) =>
-    //     handleDoc(uid, doc, type, pc, facingMode)
-    //   )
-    // )
-
   }, [])
 
-  const ourCam = useRef()
   useEffect(() => {
     openCamera(pc, facingMode, ourCam)
   }, [facingMode])
 
+  useEffect(() => {
+    console.warn(`Smile handle offer effect!`)
+    if (!remoteDesc) return
+    handleOffer(pc, remoteDesc, facingMode, onLocalDesc)
+  }, [!localDesc && remoteDesc])
+
+  useEffect(() => {
+    console.warn(`Smile handle answer effect!`)
+    if (!remoteDesc) return
+    handleAnswer(pc, remoteDesc)
+  }, [localDesc && remoteDesc])
+
+  useEffect(() => {
+    console.warn(`Smile ice candidate effect!`)
+    if (!iceCand) return
+    handleIce(pc, iceCand)
+  }, [iceCand])
+
   return (
     <div
       className={styles.smile}
-      onClick={() => createOffer(pc, offer, onOffer)}
+      onClick={() => createOffer(pc, localDesc, onLocalDesc)}
     >
       <video
         ref={theirCam}
